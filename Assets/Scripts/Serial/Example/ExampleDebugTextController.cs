864c0259f773615c885e77c9373d60ccd1abcfa0
Aymeric Faucher <aymeric.faucher@gmail.com>
2019-05-03 16:56:27 +0200

    First commit

---
+++ Assets/Scripts/Serial/Example/ExampleDebugTextController.cs
@@ -0,0 +1,26 @@
+﻿using CRI.HitBoxTemplate.Serial;
+using UnityEngine;
+
+namespace CRI.HitBoxTemplate.Example
+{
+    public class ExampleDebugTextController : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            ImpactPointControl.onImpact += OnImpact;
+        }
+
+        private void OnDisable()
+        {
+            ImpactPointControl.onImpact -= OnImpact;
+        }
+
+        private void OnImpact(object sender, ImpactPointControlEventArgs e)
+        {
+            Debug.Log(string.Format("Impact: Player [{0}], Position [{1}], Accelerometer [{2}]",
+                e.playerIndex,
+                e.impactPosition,
+                e.accelerometer));
+        }
+    }
+}
---
+++ Assets/Scripts/Serial/Example/ExampleGameController.cs
@@ -0,0 +1,30 @@
+﻿using CRI.HitBoxTemplate.Serial;
+using UnityEngine;
+
+namespace CRI.HitBoxTemplate.Example
+{
+    public class ExampleGameController : MonoBehaviour
+    {
+        [SerializeField]
+        [Tooltip("Array of the prefab of the objects that will be put at the position of the impact. Each prefab is associated with its corresponding player index.")]
+        private GameObject[] _targetPrefab;
+
+        private int _playerCount;
+
+        private void OnEnable()
+        {
+            ImpactPointControl.onImpact += OnImpact;
+        }
+
+        private void OnDisable()
+        {
+            ImpactPointControl.onImpact -= OnImpact;
+        }
+
+        private void OnImpact(object sender, ImpactPointControlEventArgs e)
+        {
+            if (e.playerIndex < _targetPrefab.Length)
+                Instantiate(_targetPrefab[e.playerIndex], e.impactPosition, Quaternion.identity);
+        }
+    }
+}
