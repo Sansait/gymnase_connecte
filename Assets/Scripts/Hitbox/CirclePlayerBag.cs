5c1a56c8ae90b0c6d1edcd074e53e5a225170070
Sklepper <klepper.samuel@gmail.com>
2020-06-23 10:28:36 +0200

    Interface Hitbox

---
+++ Assets/Scripts/Hitbox/CirclePlayerBag.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CirclePlayerBag : MonoBehaviour
+{
+	[SerializeField] private GameObject trackerPlayer;
+
+	void Update()
+	{
+		Vector3 vec = trackerPlayer.transform.position - transform.position;
+		Vector3 tmp = new Vector3(vec.magnitude * 2f, 0.1f, vec.magnitude * 2f);
+		transform.localScale = tmp;
+	}
+}
---
+++ Assets/Scripts/Hitbox/CirclePlayerBagNeg.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CirclePlayerBagNeg : MonoBehaviour
+{
+	[SerializeField] private GameObject circle;
+
+	void Update()
+	{
+		Vector3 tmp = circle.transform.localScale;
+		tmp.x -= 1;
+		tmp.z -= 1;
+		transform.localScale = tmp;
+	}
+}
+
