96be340bf0e2dd6ecd32b3c4c7d6c787f4afc6d4
Gamelab PC <marine.beurotte@edu.ece.fr>
2020-07-29 13:04:30 +0200

    Fix pong

--- Assets/Scripts/Pong/BallMovement.cs
+++ Assets/Scripts/Pong/BallMovement.cs
@@ -1,9 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using CRI.HitBoxTemplate.ScoreManager;
-using System.Security.Cryptography;
-using System.Collections.Specialized;
 
 public class BallMovement : MonoBehaviour
 {
@@ -13,7 +9,7 @@ public class BallMovement : MonoBehaviour
 	private float _speed_inc = 0.01f;
 	private float _speed;
 	private Vector3 _direction = Vector3.left;
-	private int counter = 0;
+	private float timer = 0f;
 
 	//Death variables
 	[SerializeField]
@@ -34,11 +30,11 @@ public class BallMovement : MonoBehaviour
 		this.transform.position = Vector3.zero;
 
 		_speed = 0;
-		counter = 0;
-		_direction = RotatePointAroundAxis(_direction, Random.Range(-45, 46), Vector3.up);
+		timer = 0;
+		_direction = RotatePointAroundAxis(_direction, 0, Vector3.up);
 	}
 
-	private void Collision_PlayerBar(Collision collision)
+	/*private void Collision_PlayerBar(Collision collision)
 	{
 		Vector3 contactPoint = collision.GetContact(0).point;
 		float angle = (contactPoint.z - collision.transform.position.z) * 18; // calculating the angle to bounce the ball back
@@ -47,24 +43,24 @@ public class BallMovement : MonoBehaviour
 		{
 			if (contactPoint.x > collision.transform.position.x) // ignoring collision if PlayerBar is in front of the ball
 				return;
-			_direction = RotatePointAroundAxis(Vector3.left, angle, Vector3.up);
+			_direction = RotatePointAroundAxis(Vector3.left, angle, Vector3.up).normalized;
 		}
 		else
 		{
 			if (contactPoint.x < collision.transform.position.x) // ignoring collision if PlayerBar is in front of the ball
 				return;
-			_direction = RotatePointAroundAxis(Vector3.right, angle, Vector3.down);
+			_direction = RotatePointAroundAxis(Vector3.right, angle, Vector3.down).normalized;
 		}
 		_speed += _speed_inc;
-	}
+	}*/
 
-	private void Collision_Wall(Collision collision)
+	/*private void Collision_Wall(Collision collision)
 	{
 		if (collision.gameObject.name == "Upper")
-			_direction = Vector3.Reflect(_direction, Vector3.back);
+			_direction = Vector3.Reflect(_direction, Vector3.back).normalized;
 		else if (collision.gameObject.name == "Lower")
-			_direction = Vector3.Reflect(_direction, Vector3.forward);
-	}
+			_direction = Vector3.Reflect(_direction, Vector3.forward).normalized;
+	}*/
 
 	void Score(Collision collision)
 	{
@@ -86,26 +82,26 @@ public class BallMovement : MonoBehaviour
 
 	private void OnCollisionEnter(Collision collision)
 	{
-		if (collision.gameObject.tag == "PlayerBar")
-			Collision_PlayerBar(collision);
-		else if (collision.gameObject.tag == "Wall")
-			Collision_Wall(collision);
-		else if (collision.gameObject.tag == "Goal")
+		//if (collision.gameObject.tag == "PlayerBar")
+		//	Collision_PlayerBar(collision);
+		//else if (collision.gameObject.tag == "Wall")
+		//	Collision_Wall(collision);
+		 if (collision.gameObject.tag == "Goal")
 			Score(collision);
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		if (counter < 50)
-			counter++;
-		else if (counter == 50)
+		if (timer < 2f)
+			timer += Time.deltaTime;
+		else if (timer < 5f)
 		{
 			_speed = init_speed;
-			counter++;
+			timer = 5f;
 		}
 		else
-			this.transform.position += (_direction * _speed) / Time.deltaTime;
+			this.GetComponent<Rigidbody>().MovePosition(this.transform.position + ((_direction * _speed) / Time.deltaTime));
 	}
 
 	private Vector3 RotatePointAroundAxis(Vector3 point, float angle, Vector3 axis)
