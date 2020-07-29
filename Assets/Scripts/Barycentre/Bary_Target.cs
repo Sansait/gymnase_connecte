de7369b5b37f933bb0bf821de2f8cb4ec4a367d8
Jeremykaito <jeremykaito@hotmail.fr>
2020-07-29 13:25:18 +0200

    Adding death particles barycentre

--- Assets/Scripts/Barycentre/Bary_Target.cs
+++ Assets/Scripts/Barycentre/Bary_Target.cs
@@ -6,12 +6,17 @@ namespace CRI.HitBoxTemplate.Example
 {
 	public class Bary_Target : MonoBehaviour
 	{
+		//Death variables
+		[SerializeField]
+		private DeathParticles deathParticles;
 
 		private void OnCollisionEnter(Collision collision)
 		{
 			if (collision.gameObject.tag == "Mass")
 			{
 				AudioManager.instance.Play("death");
+				deathParticles.PlayDeathParticles(this.transform.position);
+
 				Bary_Manager.Instance.score++;
 				this.gameObject.SetActive(false);
 				Bary_TargetManager.Instance.nbTargetDown++;
