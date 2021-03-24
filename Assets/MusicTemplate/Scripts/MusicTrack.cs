13da01a4fbd75bd96cc9ae2b24acf3f325b2ab57
Jeremykaito <jeremykaito@hotmail.fr>
2021-03-24 17:54:35 +0100

    Music strings prototype

---
+++ Assets/MusicTemplate/Scripts/MusicTrack.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicTrack : MonoBehaviour
+{
+    public AudioClip[] musicNotes;
+
+    private AudioSource source;
+
+    private int trackStep;
+    // Start is called before the first frame update
+    void Start()
+    {
+        trackStep = 0;
+        source = this.GetComponent<AudioSource>();
+    }
+
+    public void PlayNextNote()
+    {
+        source.clip = musicNotes[trackStep%musicNotes.Length];
+        source.Play();
+        Debug.Log(trackStep);
+        trackStep++;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(trackStep);
+        if (other.CompareTag("Player"))
+        {
+            PlayNextNote();
+        }
+    }
+}
---
+++ Assets/MusicTemplate/Scripts/StringNote.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StringNote : MonoBehaviour
+{
+    
+    private AudioSource sound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sound = this.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            sound.Play();
+        }
+    }
+}
