81039c4266b5520b77fee19e30607cf3c087ca94
Jeremykaito <jeremykaito@hotmail.fr>
2021-04-13 20:20:51 +0200

    Fx portal game

---
+++ Assets/Scripts/FGIS_Portal/MassVFXManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MassVFXManager : MonoBehaviour
+{
+    [SerializeField]
+    private ParticleSystem idleVFX;
+    [SerializeField]
+    private ParticleSystem chargingVFX;
+    [SerializeField]
+    private ParticleSystem successVFX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        IdleState();
+        chargingVFX.Stop();
+        successVFX.Stop();
+    }
+
+    public void IdleState()
+    {
+        if (idleVFX.isStopped)
+        {
+            idleVFX.Play();
+
+        }
+ 
+    }
+
+    public void ChargingState()
+    {
+        if(chargingVFX.isStopped)
+        {
+            idleVFX.Stop();
+            chargingVFX.Play();
+        }
+    }
+    public void SuccessState()
+    {
+            successVFX.Play();
+    }
+}
