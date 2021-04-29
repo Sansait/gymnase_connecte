60068f4a65c45ba8ce447a8b09276bb95959ec7d
Jeremykaito <jeremykaito@hotmail.fr>
2021-04-29 15:10:18 +0200

    FX upgrade v2
    adding vortex FX
    adding Line renderers links

--- Assets/Scripts/FGIS_Portal/PlayerMass.cs
+++ Assets/Scripts/FGIS_Portal/PlayerMass.cs
@@ -10,15 +10,28 @@ public class PlayerMass : MonoBehaviour
 	[Tooltip("Mass of the player")]
 	private float mass;
 
+	[SerializeField] LineRenderer energyLink;
+
 	private void Start()
 	{
 		_centerMass = GameObject.Find("Mass").GetComponent<CenterMass>();
+
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
 		if (_centerMass)
+        {
 			_centerMass.AddPosMass(this.transform.position, mass);
+
+			if(energyLink)
+            {
+				energyLink.gameObject.SetActive(true);
+				energyLink.SetPosition(0,_centerMass.transform.position);
+				energyLink.SetPosition(1, this.transform.position);
+			}
+		}
+
 	}
 }
